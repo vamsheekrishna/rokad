b9b0330b20dc0c2283b5d3520d02e31de3509e89
vamshee_krishna <vamshee_krishna.paleti@alcon.com>
2020-03-17 13:08:17 +0530

    fixed the mashmallow crash issue.

--- app/src/main/java/com/rokad/demo/fragments/MobileRechargePlans.java
+++ app/src/main/java/com/rokad/demo/fragments/MobileRechargePlans.java
@@ -1,27 +1,16 @@
 package com.rokad.demo.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.rokad.demo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MobileRechargePlans#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MobileRechargePlans extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -29,15 +18,6 @@ public class MobileRechargePlans extends BaseFragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MobileRechargePlans.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MobileRechargePlans newInstance(String param1, String param2) {
         MobileRechargePlans fragment = new MobileRechargePlans();
         Bundle args = new Bundle();
