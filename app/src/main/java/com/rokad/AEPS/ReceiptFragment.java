b30d14d14a10832034248cdb5b1701cea2f7b21f
pa1-teja <pavanteja.93@gmail.com>
2020-05-31 10:57:23 +0530

    addons in receipt

--- app/src/main/java/com/rokad/AEPS/ReceiptFragment.java
+++ app/src/main/java/com/rokad/AEPS/ReceiptFragment.java
@@ -2,14 +2,23 @@ package com.rokad.AEPS;
 
 import android.os.Bundle;
 
+import android.os.SystemClock;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.rokad.R;
 import com.rokad.utilities.views.BaseFragment;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class ReceiptFragment extends BaseFragment {
@@ -21,6 +30,10 @@ public class ReceiptFragment extends BaseFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private AppCompatTextView receiptNote;
+    private RecyclerView splitupView;
+    private AppCompatTextView dateText;
+    private AppCompatTextView amtInWords;
 
     public ReceiptFragment() {
         // Required empty public constructor
@@ -58,9 +71,134 @@ public class ReceiptFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_emo, container, false);
-        RecyclerView splitupView = view.findViewById(R.id.split_up_list);
-        ReceiptSplitupRecyclerAdapter splitupRecyclerAdapter = new ReceiptSplitupRecyclerAdapter();
-        splitupView.setAdapter(splitupRecyclerAdapter);
+
+
+        splitupView = view.findViewById(R.id.split_up_list);
+        splitupView.setAdapter(new ReceiptSplitupRecyclerAdapter());
+
+        amtInWords = view.findViewById(R.id.amt_in_words);
+        amtInWords.setText(new EnglishNumberToWords().convert(1000));
+
+        dateText = view.findViewById(R.id.date);
+        dateText.setText(getDate());
+
+         receiptNote = view.findViewById(R.id.recept_note);
+        receiptNote.setSelected(true);
+
+
         return view;
     }
+
+    public class EnglishNumberToWords {
+
+        final String[] tensNames = { "", " Ten", " Twenty", " Thirty", " Forty",
+                " Fifty", " Sixty", " Seventy", " Eighty", " Ninety" };
+
+        final String[] numNames = { "", " One", " Two", " Three", " Four", " Five",
+                " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen",
+                " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen" };
+
+        String convertLessThanOneThousand(int number)
+        {
+            String soFar;
+
+            if (number % 100 < 20)
+            {
+                soFar = numNames[number % 100];
+                number /= 100;
+            } else
+            {
+                soFar = numNames[number % 10];
+                number /= 10;
+
+                soFar = tensNames[number % 10] + soFar;
+                number /= 10;
+            }
+            if (number == 0)
+                return soFar;
+            return numNames[number] + " hundred" + soFar;
+        }
+
+         String convert(long number)
+        {
+            // 0 to 999 999 999 999
+            if (number == 0)
+            {
+                return "zero";
+            }
+
+            String snumber = Long.toString(number);
+
+            // pad with "0"
+            String mask = "000000000000";
+            DecimalFormat df = new DecimalFormat(mask);
+            snumber = df.format(number);
+
+            // XXXnnnnnnnnn
+            int billions = Integer.parseInt(snumber.substring(0, 3));
+            // nnnXXXnnnnnn
+            int millions = Integer.parseInt(snumber.substring(3, 6));
+            // nnnnnnXXXnnn
+            int hundredThousands = Integer.parseInt(snumber.substring(6, 9));
+            // nnnnnnnnnXXX
+            int thousands = Integer.parseInt(snumber.substring(9, 12));
+
+            String tradBillions;
+            switch (billions)
+            {
+                case 0:
+                    tradBillions = "";
+                    break;
+                case 1:
+                    tradBillions = convertLessThanOneThousand(billions) + " billion ";
+                    break;
+                default:
+                    tradBillions = convertLessThanOneThousand(billions) + " billion ";
+            }
+            String result = tradBillions;
+
+            String tradMillions;
+            switch (millions)
+            {
+                case 0:
+                    tradMillions = "";
+                    break;
+                case 1:
+                    tradMillions = convertLessThanOneThousand(millions) + " million ";
+                    break;
+                default:
+                    tradMillions = convertLessThanOneThousand(millions) + " million ";
+            }
+            result = result + tradMillions;
+
+            String tradHundredThousands;
+            switch (hundredThousands)
+            {
+                case 0:
+                    tradHundredThousands = "";
+                    break;
+                case 1:
+                    tradHundredThousands = "One Thousand ";
+                    break;
+                default:
+                    tradHundredThousands = convertLessThanOneThousand(hundredThousands) + " thousand ";
+            }
+            result = result + tradHundredThousands;
+
+            String tradThousand;
+            tradThousand = convertLessThanOneThousand(thousands);
+            result = result + tradThousand;
+            result = result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
+            result += " Only";
+            // remove extra spaces!
+            return result;
+        }
+
+    }
+
+    private String getDate(){
+       Calendar calendar = Calendar.getInstance();
+       SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+       return dateFormat.format(calendar.getTime());
+   }
 }
--- app/src/main/java/com/rokad/AEPS/ReceiptSplitupRecyclerAdapter.java
+++ app/src/main/java/com/rokad/AEPS/ReceiptSplitupRecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.rokad.R;
@@ -12,28 +13,36 @@ import com.rokad.home.MyServiceRecyclerViewAdapter;
 
 public class ReceiptSplitupRecyclerAdapter extends RecyclerView.Adapter<ReceiptSplitupRecyclerAdapter.ViewHolder> {
 
+    private int serialNumber = 1;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.receipt_split_up_aeps_list_item, parent, false);
-        return new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        holder.serialNumber.setText(String.valueOf(serialNumber++));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 2;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        AppCompatTextView serialNumber,serviceName,amount;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            serialNumber = itemView.findViewById(R.id.serial_num);
+            serviceName = itemView.findViewById(R.id.service_name_split);
+            amount = itemView.findViewById(R.id.paid_amt);
         }
     }
 }
